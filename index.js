b70ae50f12895b5cc9b474978e351f75c6d5e975
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-14 05:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function takeANumber(currline, name)
 {
   
-  return (`Welcome, ${name}. You are number ${currline} in line.`)
+  return (`Welcome, ${name}. You are number ${currline.findIndex(name)} in line.`)
   
 }
