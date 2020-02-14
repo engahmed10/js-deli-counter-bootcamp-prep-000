fdb4514055a88396e47dd1c5c47cd04e956d635b
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-14 05:30:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var currline = [];
 function takeANumber(currline, name)
 {
   
-  return (`Welcome, ${name}. You are number "1" in line.`)
+  return (`Welcome, ${name}. You are number ${1} in line.`)
   
 }
