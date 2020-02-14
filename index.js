3f1b83cdd291664698a7eb2dff076b8d3e6b6884
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-14 05:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeliLine = [];
 function takeANumber(currline, name)
 {
   var i=0;
-  return (`Welcome, ${name}. You are number ${indexOf(name)} in line.`)
+  return (`Welcome, ${name}. You are number ${} in line.`)
   
 }
